b67c7349c54c19100e0326828d319f9f681c5a03
donaghymo <504643635@qq.com>
2023-11-03 15:14:26 +0800

    加个单独的nuclei，实际上还是ssh客户端

--- apps/helloworld/main/run.py
+++ apps/helloworld/main/run.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 # encoding:utf-8
-from loguru import loggerfrom
+from loguru import logger
 
 
 async def hello_world(name):
     logger.info("[Hello World] APP 执行参数为: {name}", name=name)
     return {"status": 0, "result": "Hello," + str(name)}
-
-if __name__ == '__main__':
-    import sys
-    sys.path.append("/w5/my_tool")
-    print(ssh_client.ssh_clients.execute_remote_command("whoami"))
